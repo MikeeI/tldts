272da4719a29b9cc1ef657b0980ac15ae82d4712
Bonobo <github@in9.at>
2026-08-28 03:23:54 +0200

    fix(cli): process every positional URL

--- packages/tldts/bin/cli.js
+++ packages/tldts/bin/cli.js
@@ -7,9 +7,9 @@ const readline = require('readline');
 
 if (process.argv.length > 2) {
   // URL(s) was specified in the command arguments
-  console.log(
-    JSON.stringify(parse(process.argv[process.argv.length - 1]), null, 2),
-  );
+  for (const url of process.argv.slice(2)) {
+    console.log(JSON.stringify(parse(url), null, 2));
+  }
 } else {
   // No arguments were specified, read URLs from each line of STDIN
   const rlInterface = readline.createInterface({
